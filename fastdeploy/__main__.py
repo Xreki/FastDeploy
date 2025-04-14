fe23b3def23e94a62e3cfd583ed1c6a0d92850b6
jiangjiajun <jiangjiajun@baidu.com>
2025-04-14 09:53:20 +0800

    Init commit

---
+++ fastdeploy/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2025 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+本文件允许模块包以python -m fastdeploy方式直接执行。
+
+Authors: jiangjiajun(jiangjiajun@baidu.com)
+Date:    2025/04/14 09:53:20
+"""
+
+import sys
+from fastdeploy.cmdline import main
+sys.exit(main())
