494a607bf18c04e739e816053e0c96c6f088feef
jiangjiajun <jiangjiajun@baidu.com>
2025-04-28 20:52:16 +0800

    Optimize copyright DLTP-81007
    Change-Id: Ib034d0861e3bc292c33b391b4396513a5dc13088

--- fastdeploy/__init__.py
+++ fastdeploy/__init__.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
+
 import os
 from fastdeploy.entrypoints.llm import LLM
 from fastdeploy.engine.sampling_params import SamplingParams
