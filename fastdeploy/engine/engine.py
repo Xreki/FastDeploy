21e5b03cd590a1835852fad0b8990379ba6bd8c2
jiangjiajun <jiangjiajun@baidu.com>
2025-05-14 21:08:56 +0800

    Remove FD_MODEL_NAME and OPEN_SOURCE setting DLTP-81007
    Change-Id: I95a3da1e8720d1b41c053bbb4903a5ca6cae752f

--- fastdeploy/engine/engine.py
+++ fastdeploy/engine/engine.py
@@ -663,7 +663,10 @@ class LLMEngine(object):
                     break
                 if match := re.search(r'Loading checkpoint shards:\s*(\d+)', line):
                     self.worker_init_status["weight_loadding"] = eval(match.group(1)) * 1.0 / 100
-                elif (match := re.search(r'Start load layer (\d+)', line)) or (match := re.search(r'set state for layer (\d+)', line)):
+                elif (match := re.search(r'Start load layer (\d+)',
+                                         line)) or (match := re.search(
+                                             r'set state for layer (\d+)',
+                                             line)):
                     progress = eval(match.group(1)) * 1.0 / self.cfg.model_config.num_layers
                     self.worker_init_status["layer_loadding"] = progress
                     if self.worker_init_status["layer_loadding"] == self.cfg.model_config.num_layers - 1:
