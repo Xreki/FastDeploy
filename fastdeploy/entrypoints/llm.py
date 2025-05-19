c742c8c1f6deada76a74695afec0d355274d6a72
jiangjiajun <jiangjiajun@baidu.com>
2025-05-19 21:48:42 +0800

    fix offline inference bug DLTP-81007
    Change-Id: I8516bbe87294454450c573c118c9e2cb187792aa

--- fastdeploy/entrypoints/llm.py
+++ fastdeploy/entrypoints/llm.py
@@ -217,7 +217,7 @@ class LLM:
             finished = []
             for i, req_id in enumerate(req_ids):
                 try:
-                    for result in self.llm_engine.get_result(req_id):
+                    for result in self.llm_engine._get_generated_result(req_id):
                         result = self.llm_engine.data_processor.process_response(
                             result)
                         llm_logger.debug(
