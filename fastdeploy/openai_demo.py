54c2ce6cd4893279dd8dfb3259053d346d06dfce
gaoziyuan <gaoziyuan@baidu.com>
2025-05-27 11:54:26 +0800

    DLTP-86748 fix rl run yaml
    Change-Id: If09c9fb99ebb39ed04a9806fe7a9fd07572b01e8

--- fastdeploy/openai_demo.py
+++ fastdeploy/openai_demo.py
@@ -18,7 +18,7 @@
 import openai
 
 ip = "0.0.0.0"
-service_http_port = "9904"    # 服务配置的
+service_http_port = "9809"    # 服务配置的
 
 client = openai.Client(base_url=f"http://{ip}:{service_http_port}/v1", api_key="EMPTY_API_KEY")
 
