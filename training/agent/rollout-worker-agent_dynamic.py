bae5429971044cc11a32cb5a5bc05be476bf6e11
yushaofei01 <yushaofei01@baidu.com>
2025-06-04 21:28:01 +0800

    eb118-29991 update agent
    Change-Id: I221cc782b7a745fe373ca10f96ccda4568409ed8

--- training/agent/rollout-worker-agent_dynamic.py
+++ training/agent/rollout-worker-agent_dynamic.py
@@ -96,7 +96,7 @@ def init_child_logger(log_file: str):
 def update_weight_and_controller(job_id: str, model_path: str, model_version: str) -> None:
     """Update the weights of a model and notify the controller that it has been successfully loaded."""
     if model_path == "":
-        model_path = "/"
+        model_path = "./"
     init_child_logger(f'rollout-worker-agent_child_{device_use_id}.log')
 
     try:
