c48193b80823fea992d1f53f512e1b0bd3d3fb59
minghaipeng <minghaipeng@baidu.com>
2025-06-05 15:27:05 +0800

    DLTP-86748 add python version
    Change-Id: I28c8c69b0cd5ceb105645cfdcaad37ebd47a3948

--- tools/build_wheel_pipeline_cu123.sh
+++ tools/build_wheel_pipeline_cu123.sh
@@ -21,6 +21,10 @@ CPU_USE_BF16="false"
 CPU_USE_BF16=${2:-$CPU_USE_BF16}
 WITH_CPU="false"
 
+# python_tag
+py=$(${python} -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+python_tag="cp${py//./}"
+
 # paddle distributed use to set archs
 unset PADDLE_CUDA_ARCH_LIST
 
@@ -138,7 +142,7 @@ function build_and_install_ops() {
 
 function build_and_install() {
   echo -e "${BLUE}[build]${NONE} building fastdeploy wheel..."
-  ${python} setup.py bdist_wheel --python-tag py3
+  ${python} setup.py bdist_wheel --python-tag=${python_tag}
   if [ $? -ne 0 ]; then
     echo -e "${RED}[FAIL]${NONE} build fastdeploy wheel failed !"
     exit 1
