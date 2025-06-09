02bd3733a70d1d77bbfe7cb9f97a39968e7f6db3
liyukun04 <liyukun04@baidu.com>
2025-06-09 15:57:08 +0800

    eb118-31254 [Story] 【开源】Fastdeploy chat_template 支持-多模
    Change-Id: Ib84202fa298f42be84e5aea782e7738dbd3540d5

--- fastdeploy/input/ernie_vl_processor.py
+++ fastdeploy/input/ernie_vl_processor.py
@@ -78,7 +78,7 @@ class ErnieMoEVLProcessor(ErnieProcessor):
             stop_seqs, stop_seqs_len = self.update_stop_seq(stop_sequences)
             request.set("stop_token_ids", stop_seqs)
             request.set("stop_seqs_len", stop_seqs_len)
-
+        # messages = request.get("messages")
         # messages = parse_chat_messages(messages)
         output = self.ernie_processor.process(request)
         metadata = request.get("metadata")
