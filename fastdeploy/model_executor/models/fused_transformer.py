adfd3a3a2efdb88c701ec3d6a83ff2e3b8ae6bc5
zhoukangkang <zhoukangkang@baidu.com>
2025-06-09 19:33:48 +0800

    [wenxin-tools-325] add triton
    Change-Id: I9b0142120c41a5364c86dd246d7822d77634d0db

--- fastdeploy/model_executor/models/fused_transformer.py
+++ fastdeploy/model_executor/models/fused_transformer.py
@@ -370,7 +370,8 @@ class FusedTransformer(nn.Layer):
                         top_k=inference_args.moe_config.top_k,
                         moe_use_gate_correction_bias=inference_args.moe_config.
                         moe_use_gate_correction_bias,
-                        moe_quant_type=inference_args.moe_config.moe_quant_type,
+                        moe_quant_type=inference_args.moe_config.
+                        moe_quant_type,
                         layer_idx=i,
                         gate_weight_key=f"ernie.layers.{i}.mlp.gate.weight",
                         gate_correction_bias_key=
