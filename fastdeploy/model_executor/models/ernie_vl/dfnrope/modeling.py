b2bab7e4e182af590f80f984d9cce6bcc248dc75
gaoziyuan <gaoziyuan@baidu.com>
2025-06-10 22:08:16 +0800

    DLTP-86748 optimize train&infer reshard
    Change-Id: I9c2512506677bf20a2c69bdfdb30dacdc2c3b2c7

--- fastdeploy/model_executor/models/ernie_vl/dfnrope/modeling.py
+++ fastdeploy/model_executor/models/ernie_vl/dfnrope/modeling.py
@@ -468,7 +468,7 @@ class DFNRopeVisionTransformerPretrainedModel(PretrainedModel):
             vit_names.append(f"vision_model.blocks.{layer_idx}.mlp.fc2.bias")
 
         for train_name in vit_names:
-            infer_to_train[train_name[len("vision_model."):]] = train_name
+            infer_to_train[train_name] = train_name
         
         return infer_to_train
     
