35725b24caa9d57e6b28d67c4b2fc72475a2aa12
liyukun04 <liyukun04@baidu.com>
2025-06-12 17:36:07 +0800

    eb118-31254 [Story] 【开源】Fastdeploy chat_template 支持-多模
    Change-Id: I5fb7898a2511f8cae8c0fcf2e70cde8dd83a1d3c

--- fastdeploy/input/mm_processor/utils/io_utils.py
+++ fastdeploy/input/mm_processor/utils/io_utils.py
@@ -43,7 +43,7 @@ def file_download(url, download_dir, save_to_disk=False, retry=0, retry_interval
         save_to_disk: 是否保存在本地路径
 
     """
-    from .video_utils import VideoReaderWrapper
+    from data_processor.utils.video_utils import VideoReaderWrapper
 
     if isinstance(url, Image.Image):
         return url
