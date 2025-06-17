9a85abd062e431f6867a046aa3c3e5f6ccd9e741
wangmingkai02 <wangmingkai02@baidu.com>
2025-06-17 18:11:29 +0800

    Avoid breakgraph caused by IMPORT_NAME in SOT DLTP-86748
    Change-Id: I2e475e1b0da53620ec68ee65bd83585fc17eda36

--- fastdeploy/model_executor/layers/attention/ops/append_attention.py
+++ fastdeploy/model_executor/layers/attention/ops/append_attention.py
@@ -14,9 +14,15 @@
 # limitations under the License.
 """
 
-import paddle
 from typing import Optional
+
+import paddle
+
 from fastdeploy.platforms import current_platform
+
+if current_platform.is_cuda():
+    from fastdeploy.model_executor.ops.gpu import \
+        append_attention as append_attention_gpu
 
 
 def append_attention(
@@ -74,8 +80,7 @@ def append_attention(
     Returns:
     """
     if current_platform.is_cuda():
-        from fastdeploy.model_executor.ops.gpu import append_attention
-        out = append_attention(
+        out = append_attention_gpu(
             qkv,
             key_cache,
             value_cache,
